6a7afe5274fdb01e2fbfccb63ee720166d064c32
JenniferFighting <jen.bini@gmail.com>
2023-06-22 15:44:39 +0200

    vippage

---
+++ vip.php
@@ -0,0 +1,64 @@
+<?php      require_once 'config/function.php';
+           require_once 'inc/header.inc.php';
+            ?>
+
+
+    <link rel="stylesheet" href="assets/css/styles.css">
+
+
+        <section class="bkg" >
+            
+        <div class="title">
+            <h1>DEVENIR VIP</h1>
+        </div>
+
+        <div class="title">
+            <h2>VIP</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores sunt recusandae placeat et enim! Unde consectetur libero quis atque soluta velit quia, provident eos molestias beatae fugiat quo vitae incidunt.</p>
+
+            <div id="player1">
+            <img src="assets/pictures/Perso1-removebg-preview.png" alt="player1">
+            </div>
+        </div>
+
+        <div class="title">
+            <h2>VIP +</h2>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus pariatur laboriosam doloremque voluptatibus eius quaerat commodi libero omnis dolore placeat, expedita maiores culpa</p>
+
+            <div id="player2">
+            <img src= "assets/pictures/Perso2-removebg-preview.png" alt="player2">
+            </div>
+        </div>
+
+        <div class="socialMedia">
+        <a href="" class="icon_social"><img src="assets/pictures/instagram_color.png" alt="instagram"></a>
+        <a href="" class="icon_social"><img src="assets/pictures/facebook (3).png" alt="facebook"></a>
+        <a href="" class="icon_social"><img src="assets/pictures/tik-tok.png" alt="tiktok"></a>
+
+        <img src="assets/pictures/logo_discord.png" alt="discord" id="discord">
+
+        <a href="" class="icon_social"><img src="assets/pictures/twitter.png" alt="twitter"></a>
+        <a href="" class="icon_social"><img src="assets/pictures/twitch.png" alt="twitch"></a>
+        <a href="" class="icon_social"><img src="assets/pictures/youtube.png" alt="youtube"></a>
+</div>
+        </section>
+
+<footer>
+    <div class="contact">
+
+    <img src="assets\pictures\icone-de-messagerie.png" alt="mail">
+        <p>Contact</p> 
+
+        <img src="assets\pictures\V.jpg" alt="gtaV">
+
+        <img src="assets\pictures\18.png" alt="under18">
+
+        </div> 
+</footer>
+
+
+
+
+
+
+        <?php     require_once 'inc/footer.inc.php';          ?>
