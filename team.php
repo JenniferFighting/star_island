e239dda4da5721022962c24638093c2a2a4f54ad
JenniferFighting <jen.bini@gmail.com>
2023-06-23 11:24:47 +0200

    avatar

---
+++ team.php
@@ -0,0 +1,55 @@
+<?php      require_once 'config/function.php';
+           require_once 'inc/header.inc.php';
+            ?>
+
+
+    <link rel="stylesheet" href="assets/css/style_team.css">
+
+    <div class="title">
+            <h1>L'EQUIPE</h1>
+        </div>
+
+    <img src="assets/pictures/ellios 4.png" class="rounded-circle mb-3" style="width: 150px;"
+  alt="Avatar"/>
+
+<h5 class="mb-2"><strong>John Doe</strong></h5>
+<p class="text-muted">Web designer <span class="badge bg-primary">PRO</span></p>
+
+<img src="assets/pictures/Souen 4.png" class="rounded-circle mb-3" style="width: 150px;"
+  alt="Avatar"/>
+
+<h5 class="mb-2"><strong>Jane Doe</strong></h5>
+<p class="text-muted">Developpeuse <span class="badge bg-primary">PRO</span></p>
+
+
+
+
+<div class="socialMedia">
+        <a href="" class="icon_social"><img src="assets/pictures/instagram_color.png" alt="instagram"></a>
+        <a href="" class="icon_social"><img src="assets/pictures/facebook (3).png" alt="facebook"></a>
+        <a href="" class="icon_social"><img src="assets/pictures/tik-tok.png" alt="tiktok"></a>
+
+        <img src="assets/pictures/logo_discord.png" alt="discord" id="discord">
+
+        <a href="" class="icon_social"><img src="assets/pictures/twitter.png" alt="twitter"></a>
+        <a href="" class="icon_social"><img src="assets/pictures/twitch.png" alt="twitch"></a>
+        <a href="" class="icon_social"><img src="assets/pictures/youtube.png" alt="youtube"></a>
+</div>
+        </section>
+
+<footer>
+    <div class="contact">
+
+    <img src="assets\pictures\icone-de-messagerie.png" alt="mail">
+        <p>Contact</p> 
+
+        <img src="assets\pictures\V.jpg" alt="gtaV">
+
+        <img src="assets\pictures\18.png" alt="under18">
+
+        </div> 
+</footer>
+
+
+
+<?php     require_once 'inc/footer.inc.php';          ?>
